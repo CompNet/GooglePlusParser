6e6060c9cd69b526c974ac895e1f5201f53d261f
Vincent Labatut <vince.labatut@gmail.com>
2013-07-03 10:06:08 +0300

    R script modifications

--- res/rscripts/3.AnalyzeNetwork.R
+++ res/rscripts/3.AnalyzeNetwork.R
@@ -48,8 +48,75 @@ com.table <- as.matrix(read.table(com.file))
 ##################################################
 # process degrees
 ##################################################
-for(mode in c("all","in","out"))
-{	p <- degree(g,mode=mode)
+for(mode in c("all","in","out","knn"))
+{	gc()
+	if(mode=="knn")
+		p <- graph.knn(graph=g)$knn
+	else
+		p <- degree(g,mode=mode)
+	rowname <- paste("degree.",mode,sep="")
+	write.table(x=p,file=paste(folder,prefix,".",rowname,".txt",sep=""),row.names=FALSE,col.names=FALSE)
+	temp <- rownames(prop.table)
+	if(length(which(temp==rowname))==0)
+	{	prop.table <- rbind(prop.table,c(mean(p),sd(p)))
+		rownames(prop.table) <- c(temp,rowname)
+	}
+	else
+	{	prop.table[rowname,"mean"] <- mean(p)
+		prop.table[rowname,"stdev"] <- sd(p)
+	}
+}
+write.table(x=prop.table,file=prop.file)
+
+
+##################################################
+# process distances
+##################################################
+for(mode in c("undirected","directed"))
+{	gc()
+	if(mode=="undirected")
+		p <- shortest.paths(graph=g,mode="all")
+	else
+		p <- shortest.paths(graph=g,mode="out")
+	rowname <- paste("degree.",mode,sep="")
+	write.table(x=p,file=paste(folder,prefix,".",rowname,".txt",sep=""),row.names=FALSE,col.names=FALSE)
+	temp <- rownames(prop.table)
+	if(length(which(temp==rowname))==0)
+	{	prop.table <- rbind(prop.table,c(mean(p),sd(p)))
+		rownames(prop.table) <- c(temp,rowname)
+	}
+	else
+	{	prop.table[rowname,"mean"] <- mean(p)
+		prop.table[rowname,"stdev"] <- sd(p)
+	}
+}
+write.table(x=prop.table,file=prop.file)
+
+##################################################
+# process diameters
+##################################################
+for(mode in c("directed", "undirected"))
+{	if(mode=="undirected")
+		p <- diameter(graph=g, directed=FALSE)
+	else
+		p <- diameter(graph=g, directed=TRUE)
+	rowname <- paste("diameter.",mode,sep="")
+	prop.table[rowname,"mean"] <- p
+	prop.table[rowname,"stdev"] <- 0
+}
+write.table(x=prop.table,file=prop.file)
+
+##################################################
+# process centralities
+##################################################
+for(mode in c("betweenness.directed","betweenness.undirected","closeness"))
+{	gc()
+	if(mode=="betweenness.undirected")
+		p <- betweenness(graph=g,directed=FALSE) / vcount(g)
+	else if(mode=="betweenness.directed")
+		p <- betweenness(graph=g,directed=TRUE) / vcount(g)
+	else if(mode=="closeness")
+		p <- closeness(graph=g, mode="all")
 	rowname <- paste("degree.",mode,sep="")
 	write.table(x=p,file=paste(folder,prefix,".",rowname,".txt",sep=""),row.names=FALSE,col.names=FALSE)
 	temp <- rownames(prop.table)
@@ -104,10 +171,22 @@ write.table(x=com.table,file=com.file)
 
 # external louvain
 # cd /home/vlabatut/eclipse/workspaces/Networks/CommunityDetection/algorithms/louvain/
-# ./convert.out -i ~/eclipse/workspaces/Extraction/Database/googleplus/giantcomp.edgelist -o graph.bin
-# ./community.out graph.bin -l -1 -q 0.0001 > graph.tree
+# convert edgelist to binary:	./convert.out -i ~/eclipse/workspaces/Extraction/Database/googleplus/giantcomp.edgelist -o graph.bin
+# detect communities: 			./community.out graph.bin -v -l -1 -v > graph.tree
+# faster detection:				./community.out graph.bin -v -l -1 -q 0.0001 > graph.tree
+# display resulting tree:		./hierarchy.out graph.tree
 
 # external hierarchical infomap
 # cd /home/vlabatut/eclipse/workspaces/Networks/CommunityDetection/algorithms/infohiermap/directed/
 # ./infomap.out 1 ~/eclipse/workspaces/Extraction/Database/googleplus/giantcomp.net 1 1
 
+# external infomap
+# cd /home/vlabatut/eclipse/workspaces/Networks/CommunityDetection/algorithms/infomap/directed
+# ./infomap.out 1 ~/eclipse/workspaces/Extraction/Database/googleplus/giantcomp.net 1
+
+# external copra
+# cd /home/vlabatut/eclipse/workspaces/Networks/CommunityDetection/algorithms/copra
+# java -Xms31g -Xmx31g -cp copra.jar COPRA ~/eclipse/workspaces/Extraction/Database/googleplus/giantcomp.edgelist
+
+# external MCL
+# mcl ~/eclipse/workspaces/Extraction/Database/googleplus/giantcomp.edgelist -I 2.0
